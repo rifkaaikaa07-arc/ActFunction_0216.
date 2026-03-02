4e41be92fb685fced45eb1eebd2e340841b6c9d9
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-02 22:08:41 +0700

    menambahkan output

--- challange.cpp
+++ challange.cpp
@@ -17,3 +17,8 @@ float prosedurhitungLuasKetupat(float d_1, float d_2){
 void proseduroutput (){
     cout << "LuasKetupat : " << prosedurhitungLuasKetupat(d_1, d_2) << endl;
 }
+
+int main (){
+    prosedurinputdata();
+    proseduroutput();
+}
