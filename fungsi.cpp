ae259ae3f34c3bca2fe1274d2f2d5a320e0a7d48
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-02 21:56:07 +0700

    menambahkan fungsi input p dan l

---
+++ fungsi.cpp
@@ -0,0 +1,13 @@
+//library
+#include <iostream>
+using namespace std;
+//deklarasi variabel
+int p, l;
+
+//implementasi fungsi dan prosedur
+void input (){
+    cout << "masukkan panjang : ";
+    cin >> p;
+    cout << "masukkan lebar : ";
+    cin >> l;
+}
